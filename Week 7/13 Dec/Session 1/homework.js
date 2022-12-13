bd08f78c385d96022f091ebc28664893b6129fc7
TheTechDope <thetechdope.in@gmail.com>
2022-12-13 13:35:50 +0530

    Material added till 13 Dec (Session 1)

---
+++ Week 7/13 Dec/Session 1/homework.js
@@ -0,0 +1,12 @@
+/*
+
+Today's Homework -
+
+1. Add Dropdown for Payment Mode in Currency Selector Application
+2. Read about Controlled v/s Uncontrolled Components
+
+   Link - https://goshacmd.com/controlled-vs-uncontrolled-inputs-react/
+
+3. Read about Render Props
+
+*/
